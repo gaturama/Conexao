b871b9ab886394969da434b08c0aaaa737fec0e8
gaturama <bielsobv@gmail.com>
2022-11-09 22:13:16 -0300

    adição de comandos no programa

---
+++ Conexao.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Conexao{
+    public static void main (String[] args) {
+        try{
+            Connection conexao = DAO.createConnection();
+            
+        }
+    }
+}
