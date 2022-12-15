aa8f5366fd42b304c0bdded2f03df4c1271c4500
Gabito <101840148+gaturama@users.noreply.github.com>
2022-12-15 08:13:42 -0300

    Update DAO.java

--- DAO.java
+++ DAO.java
@@ -1,8 +1,31 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public abstract class DAO {
     
-    private Connection connection;
-
-    public void setConnection() {
-        this.connection = connection;
+    private static Connection connection;
+    
+    public static Connection createConnection(){
+        try{
+            final String URL = "";
+            final String USER = "root":
+            final String PASSWORD = "";
+            
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            
+            return connection;
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static void closeConnection(){
+        try{
+            connection.close();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
